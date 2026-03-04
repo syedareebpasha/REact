e16afcd4dc93876b70fb248bcf7d44935fae2704
syedareebpasha <syedareebpasha9999@gmail.com>
2026-03-04 06:47:59 +0500

    chanes

--- vite-project/src/Layout/Footer.jsx
+++ vite-project/src/Layout/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Footer.css'
 import logo from '../assets/logo.png'
+import facebook from '../assets/facebooklogo.png'
+import twitter from '../assets/twitterlogo.png'
+import instagram from '../assets/instagramlogo.png'
+import linkedin from '../assets/linkedinlogo.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div>  
-       <div>
+       <div style={{  display:'flex', flexDirection:'column'  }}>
          <img src={logo} alt="Logo" className="footer-logo" />
+         <h6 style={{  marginTop:'-10px', marginLeft:'10px', fontSize:'15px', fontWeight:'bold'  }}>Beyond Mile's</h6>
        </div>
         
 {/* <div>Font awesome</div> */}
@@ -18,7 +23,14 @@ const Footer = () => {
     <li><a href="">Latest Stories</a></li>
     <li><a href="">Trekker's Highlights</a></li>
     <li><a href="">Service</a></li></div>
-    <div>Follow Us On</div>
+    <div> <p style={{marginLeft:'35px', fontWeight:'bold', fontSize:'20px'}}>Follow Us On </p>
+<div className='social-icons'>
+  <img src={facebook} alt="Facebook" className="social-icon1" />
+  <img src={twitter} alt="Twitter" className="social-icon" />
+  <img src={instagram} alt="Instagram" className="social-icon1" />
+  <img src={linkedin} alt="LinkedIn" className="social-icon" />
+</div>
+    </div>
           </div>
   )
 }
--- vite-project/src/Layout/Navbar.jsx
+++ vite-project/src/Layout/Navbar.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import Button from '../Shared/Button'
 import './Navbar.css'
+import search from '../assets/search.svg'
 const Navbar = () => {
   return (
     <div className='Main-div'>
     <div><img className='logo' src={logo} alt="" /></div>
 <div>
   <ul className='Nav-links'>
-    <li><a href="">Home</a></li>
-    <li><a href="">Top Destinations</a></li>
-    <li><a href="">Latest Stories</a></li>
-    <li><a href="">Trekker's Highlights</a></li>
-    <li><a href="">Service</a></li>
+    <li className='nav'><a href="">Top Destinations</a></li>
+    <li className='nav'><a href="">Latest Stories</a></li>
+    <li className='nav'><a href="">Home</a></li>
+    <li className='nav'><a href="">Trekker's Highlights</a></li>
+    <li className='nav'><a href="">Service</a></li>
   </ul>
 </div>
-<div className='btn'>
+<div style={{ display:'flex', gap:'15px'}} className='btn'>
+  <img style={{ width:'30px', height:'30px', marginTop:'10px' }} src={search} alt="Search" />
   <Button className="custom-button" text="Search" />
   </div>
     </div>
